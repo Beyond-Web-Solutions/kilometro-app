0c27b6d7be6b3bb041fc193c08f882a8e8362549
MeesEgberts <mees.egberts@beyond-web.nl>
2025-02-06 13:30:26 +0100

    feat: update eas.json to include APP_VARIANT for environment configurations and modify maps.ts to use APP_VARIANT for Google Maps API key retrieval

--- src/utils/maps.ts
+++ src/utils/maps.ts
@@ -1,7 +1,7 @@
 import { Platform } from "react-native";
 
 export function getGoogleMapsApiKey() {
-  if (process.env.EAS_BUILD_PROFILE === "production") {
+  if (process.env.APP_VARIANT === "production") {
     if (Platform.OS === "ios") {
       return process.env.EXPO_PUBLIC_GOOGLE_MAPS_API_KEY_IOS;
     }
